5c7b74178bfe5fce67cd25f2744f8f62aa841b3a
rohitharidas123 <42977203+rohitharidas123@users.noreply.github.com>
2018-09-25 13:36:49 -0400

    Add files via upload

---
+++ src/TallyCounter.java
@@ -0,0 +1,19 @@
+
+public class TallyCounter 
+{
+	//fields
+	private int count;
+	
+	
+	//constructors
+	public TallyCounter()
+	{
+		count = 0;
+	}
+	
+	
+	public void click()
+	{
+		count++
+	}
+}
